308a27e6b5dc4da6158a7260b06581fb4571225b
Lejla Hećo <hecolejla@hotmail.com>
2021-12-10 12:43:23 +0100

    Task 42 - logiranje korisnika
    Slanje podataka za prijavu na backend

--- eRestoranAngular/src/app/login/login.component.ts
+++ eRestoranAngular/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {MyConfig} from "../my-config";
+import {Login} from "./view-models/login-vm";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  prijava : Login = null;
+  constructor(private httpKlijent : HttpClient, private router : Router) { }
 
   ngOnInit(): void {
   }
 
+  posaljiPodatke() {
+    this.httpKlijent.post(MyConfig.adresaServera+'/Autentifikacija/Login',this.prijava).subscribe((response : any)=>{
+      localStorage.setItem("autentifikacija-token", response)
+
+      if (response!="greska") {
+        //redirect na login putanju
+        this.router.navigateByUrl("/login");
+      }
+      else {
+        alert("pogresan username i/ili password");
+      }
+      }
+    )
+  }
 }
