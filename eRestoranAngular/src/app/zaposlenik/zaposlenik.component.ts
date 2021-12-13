0fb9189dec873a0e410014105fa1efd5509de9ad
hanabajric <hnbajric@gmail.com>
2021-12-13 19:18:23 +0100

    Task 83-Urađivanje panela zaposlenika

--- eRestoranAngular/src/app/zaposlenik/zaposlenik.component.ts
+++ eRestoranAngular/src/app/zaposlenik/zaposlenik.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {MeniStavka} from "../meni/view-models/meni-stavka-vm";
+import {Zaposlenik} from "./view-models/zaposlenik-vm";
+import {MyConfig} from "../my-config";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-zaposlenik',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./zaposlenik.component.css']
 })
 export class ZaposlenikComponent implements OnInit {
-
-  constructor() { }
+  zaposlenici : Zaposlenik[] = null;
+  constructor(private httpKlijent:HttpClient) { }
 
   ngOnInit(): void {
+    this.ucitajZaposlenike();
+  }
+  public ucitajZaposlenike() {
+    this.httpKlijent.get("https://localhost:44325"+"/Zaposlenik/GetAllPaged").subscribe((result : any)=>{
+      this.zaposlenici = result;
+    })
   }
 
 }
