f668ca767e23190af0622f291027d7fd1abb9910
Lejla Hećo <hecolejla@hotmail.com>
2022-01-02 02:17:44 +0100

    Task 218 - API funkcionalnost za deaktivaciju korisničkog profila

--- FIT_Api_Examples/FIT_Api_Examples/ModulKorisnik/Controllers/KorisnikController.cs
+++ FIT_Api_Examples/FIT_Api_Examples/ModulKorisnik/Controllers/KorisnikController.cs
@@ -1,7 +1,10 @@
 ﻿using FIT_Api_Examples.Data;
 using FIT_Api_Examples.Helper.AutentifikacijaAutorizacija;
+using FIT_Api_Examples.ModulAutentifikacija.Models;
 using FIT_Api_Examples.ModulKorisnik.Models;
 using FIT_Api_Examples.ModulKorisnik.ViewModels;
+using FIT_Api_Examples.ModulNarudzba.Models;
+using FIT_Api_Examples.ModulRezervacija.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -37,6 +40,40 @@ namespace FIT_Api_Examples.ModulKorisnik.Controllers
             _dbContext.Korisnik.Add(noviKorisnik);
             _dbContext.SaveChanges();
             return Ok(noviKorisnik.ID);
+        }
+        [HttpGet]
+        public ActionResult Delete()
+        {
+            if (!HttpContext.GetLoginInfo().isPermisijaKorisnik)
+                return BadRequest("nije logiran");
+
+            Korisnik korisnik = HttpContext.GetLoginInfo().korisnickiNalog.Korisnik;
+            List<Narudzba> trenutneNarudzbe = _dbContext.Narudzba.Where(n => n.KorisnikID == korisnik.ID && n.StatusNarudzbeID != 6 && n.Zakljucena).ToList();
+            if (trenutneNarudzbe != null && trenutneNarudzbe?.Count != 0)
+                return BadRequest("Trenutno ne mozete deaktivirati profil jer su Vase narudzbe u izradi");
+
+            List<KorisnikKupon> kuponiKorisnika = _dbContext.KorisnikKupon.Where(kk => kk.KorisnikID == korisnik.ID).ToList();
+            List<Narudzba> narudzbe = _dbContext.Narudzba.Where(n => n.KorisnikID == korisnik.ID).ToList();
+            List<StavkaNarudzbe> stavkeNarudzbi = new List<StavkaNarudzbe>();
+            foreach(Narudzba narudzba in narudzbe)
+            {
+                stavkeNarudzbi.AddRange(_dbContext.StavkaNarudzbe.Where(sn => sn.NarudzbaID == narudzba.ID).ToList());
+            }
+            List<Rezervacija> rezervacije = _dbContext.Rezervacija.Where(r => r.KorisnikID == korisnik.ID).ToList();
+            List<OmiljenaStavka> omiljeneStavke = _dbContext.OmiljenaStavka.Where(os => os.KorisnikID == korisnik.ID).ToList();
+            List<AutentifikacijaToken> logovi = _dbContext.AutentifikacijaToken.Where(at => at.KorisnickiNalogId == korisnik.ID).ToList();
+
+            _dbContext.KorisnikKupon.RemoveRange(kuponiKorisnika);
+            _dbContext.StavkaNarudzbe.RemoveRange(stavkeNarudzbi);
+            _dbContext.Narudzba.RemoveRange(narudzbe);
+            _dbContext.Rezervacija.RemoveRange(rezervacije);
+            _dbContext.OmiljenaStavka.RemoveRange(omiljeneStavke);
+            _dbContext.AutentifikacijaToken.RemoveRange(logovi);
+            _dbContext.Korisnik.Remove(korisnik);
+            _dbContext.KorisnickiNalog.Remove(korisnik);
+            _dbContext.SaveChanges();
+
+            return Ok();
         }
         [HttpGet]
         public List<Korisnik> GetAll()
