3a497fae40a5cbb0c608b8c84d0e3edb2f09ebab
hanabajric <hnbajric@gmail.com>
2022-01-16 15:52:27 +0100

    Task 249 - Validiranje formi dostavljača

--- eRestoranAngular/src/app/dostavljac/edit-dostavljac/edit-dostavljac.component.ts
+++ eRestoranAngular/src/app/dostavljac/edit-dostavljac/edit-dostavljac.component.ts
@@ -20,6 +20,8 @@ export class EditDostavljacComponent implements OnInit {
   closeModal : boolean = false;
   obavjestenjeNaslov : string = "";
   obavjestenjeSadrzaj : string = "";
+
+  fieldText:boolean;// sakrivanje lozinke
   constructor( private httpKlijent :HttpClient ,private route: ActivatedRoute,private router:Router) { }
 
   ngOnInit(): void {
@@ -50,22 +52,58 @@ export class EditDostavljacComponent implements OnInit {
 
 
     posaljiPodatke() {
-  // @ts-ignore
-  var file = document.getElementById("fajl-input").files[0];
+    if(this.validirajFormu()) {
+      // @ts-ignore
+      var file = document.getElementById("fajl-input").files[0];
 
-  var data = new FormData();
-  data.append("slikaDostavljaca", file);
-  this.httpKlijent.post(MyConfig.adresaServera + "/Dostavljac/Update/"+ this.id, this.urediDostavljac).subscribe((result :any)=>{
-  this.httpKlijent.post(MyConfig.adresaServera + "/Dostavljac/AddSlika/" + result, data).subscribe((result: any)=>{
-    this.obavjestenje = true;
-    this.closeModal = false;
-    this.obavjestenjeNaslov = "Ažuriranje podataka uspješno";
-    this.obavjestenjeSadrzaj = "Uspješno ste uredili podatke o dostavljaču "+ this.urediDostavljac.ime+" "+this.urediDostavljac.prezime;
- // this.router.navigate(['/dostavljac']);
-});
-});
+      var data = new FormData();
+      data.append("slikaDostavljaca", file);
+      this.httpKlijent.post(MyConfig.adresaServera + "/Dostavljac/Update/" + this.id, this.urediDostavljac).subscribe((result: any) => {
+        this.httpKlijent.post(MyConfig.adresaServera + "/Dostavljac/AddSlika/" + result, data).subscribe((result: any) => {
+          this.prikaziObavjestenje("Ažuriranje podataka uspješno","Uspješno ste uredili podatke o dostavljaču " + this.urediDostavljac.ime + " " + this.urediDostavljac.prezime);
+
+
+        });
+      });
+    }
+    else {
+     this.prikaziObavjestenje("Neadekvatno ispunjena forma za promjenu ličnih podataka","Molimo ispunite sva obavezna polja, pa pokušajte ponovo.");
+    }
 
 }
+  private prikaziObavjestenje(naslov : string, sadrzaj : string) {
+    this.obavjestenje = true;
+    this.closeModal = false;
+    this.obavjestenjeNaslov = naslov;
+    this.obavjestenjeSadrzaj = sadrzaj;
+  }
+
+  validirajFormu() : boolean{
+    // @ts-ignore
+    //var slika = document.getElementById("fajl-input").files[0];
+    return this.urediDostavljac.korisnickoIme != null && this.urediDostavljac.korisnickoIme?.length > 0
+      && this.urediDostavljac.lozinka != null && this.urediDostavljac.lozinka?.length > 0
+      && this.urediDostavljac.ime != null && this.urediDostavljac.ime?.length > 0
+      && this.urediDostavljac.prezime != null && this.urediDostavljac.prezime?.length > 0
+      && this.urediDostavljac.email != null && this.urediDostavljac.email?.length > 0
+      && this.urediDostavljac.slika != null
+
+  }
+
+  provjeriPolje(polje: any) {
+    if (polje.invalid && (polje.dirty || polje.touched)){
+      if (polje.errors?.['required']){
+        return 'Niste popunili ovo polje!';
+      }
+      else {
+        return '';
+      }
+    }
+    return 'Obavezno polje za unos';
+  }
+  prikaziSakrij() {
+    this.fieldText = !this.fieldText;
+  }
   animirajObavjestenje() {
     return this.closeModal == true? 'animate__animated animate__bounceOut' : 'animate__animated animate__bounceIn';
   }
--- eRestoranAngular/src/app/dostavljac/novi-dostavljac/novi-dostavljac.component.ts
+++ eRestoranAngular/src/app/dostavljac/novi-dostavljac/novi-dostavljac.component.ts
@@ -15,6 +15,8 @@ noviDostavljac:NoviDostavljac= new NoviDostavljac();
   closeModal : boolean = false;
   obavjestenjeNaslov : string = "";
   obavjestenjeSadrzaj : string = "";
+
+  fieldText: boolean;// za sakrivanje passworda
   constructor(private httpKlijent: HttpClient) { }
 
   ngOnInit(): void {
@@ -34,7 +36,7 @@ noviDostavljac:NoviDostavljac= new NoviDostavljac();
 
   posaljiPodatke() {
 
-
+if(this.validirajFormu()){
     // @ts-ignore
     var file = document.getElementById("fajl-input").files[0];
     var data = new FormData();
@@ -44,12 +46,57 @@ noviDostavljac:NoviDostavljac= new NoviDostavljac();
       this.httpKlijent.post(MyConfig.adresaServera + "/Dostavljac/AddSlika/" + result, data).subscribe((result:any)=>{
         this.obavjestenje = true;
         this.closeModal = false;
-        this.obavjestenjeNaslov = "Ažuriranje podataka uspješno";
-        this.obavjestenjeSadrzaj = "Uspješno ste uredili podatke o dostavljaču "+ this.noviDostavljac.ime+" "+this.noviDostavljac.prezime;
+        this.obavjestenjeNaslov = "Dodavanje podataka uspješno";
+        this.obavjestenjeSadrzaj = "Uspješno ste dodali dostavljača "+ this.noviDostavljac.ime+" "+this.noviDostavljac.prezime;
         this.ocistiFormu();
       });
     });
+}
+else{
+  this.obavjestenje = true;
+  this.closeModal = false;
+  this.obavjestenjeNaslov = "Neadekvatno ispunjena forma za promjenu ličnih podataka";
+  this.obavjestenjeSadrzaj = "Molimo ispunite sva obavezna polja, pa ponovo pokušajte";
+}
 
+  }
+
+  /*private validirajFormu() {
+    var osnovneInformacije : boolean = this.noviDostavljac.username != null && this.noviDostavljac.username?.length > 0
+      && this.noviDostavljac.password != null && this.noviDostavljac.password?.length > 0
+      && this.noviDostavljac.ime != null && this.noviDostavljac.ime?.length > 0
+      && this.noviDostavljac.prezime != null && this.noviDostavljac.prezime?.length > 0
+      && this.noviDostavljac.email != null && this.noviDostavljac.email?.length > 0;
+    var dodatneInformacije : boolean = true;
+      dodatneInformacije = document.getElementById('preview-slika').getAttribute("src").length > 0;
+    return osnovneInformacije && dodatneInformacije;
+  }*/
+
+  validirajFormu() : boolean{
+    // @ts-ignore
+    var slika = document.getElementById("fajl-input").files[0];
+    return this.noviDostavljac.username != null && this.noviDostavljac.username?.length > 0
+      && this.noviDostavljac.password != null && this.noviDostavljac.password?.length > 0
+      && this.noviDostavljac.ime != null && this.noviDostavljac.ime?.length > 0
+      && this.noviDostavljac.prezime != null && this.noviDostavljac.prezime?.length > 0
+      && this.noviDostavljac.email != null && this.noviDostavljac.email?.length > 0
+      && slika != null
+
+  }
+
+  provjeriPolje(polje: any) {
+    if (polje.invalid && (polje.dirty || polje.touched)){
+      if (polje.errors?.['required']){
+        return 'Niste popunili ovo polje!';
+      }
+      else {
+        return '';
+      }
+    }
+    return 'Obavezno polje za unos';
+  }
+  prikaziSakrij() {
+    this.fieldText = !this.fieldText;
   }
   ocistiFormu(){
     this.noviDostavljac.ime = null;
