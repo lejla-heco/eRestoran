429bd975150d9a459771979a34a4392ac71df46a
hanabajric <hnbajric@gmail.com>
2022-01-16 16:10:00 +0100

    Task 250 - Validiranje formi zaposlenika

--- eRestoranAngular/src/app/zaposlenik/edit-zaposlenik/edit-zaposlenik.component.ts
+++ eRestoranAngular/src/app/zaposlenik/edit-zaposlenik/edit-zaposlenik.component.ts
@@ -18,6 +18,7 @@ export class EditZaposlenikComponent implements OnInit {
   closeModal : boolean = false;
   obavjestenjeNaslov : string = "";
   obavjestenjeSadrzaj : string = "";
+  fieldText:boolean;
   constructor( private httpKlijent :HttpClient ,private route: ActivatedRoute,private router:Router) { }
 
   ngOnInit(): void {
@@ -46,6 +47,7 @@ export class EditZaposlenikComponent implements OnInit {
   }
 
   posaljiPodatke() {
+    if(this.validirajFormu()){
     // @ts-ignore
     var file = document.getElementById("fajl-input").files[0];
 
@@ -53,12 +55,44 @@ export class EditZaposlenikComponent implements OnInit {
     data.append("slikaZaposlenika", file);
     this.httpKlijent.post(MyConfig.adresaServera + "/Zaposlenik/Update/"+ this.id, this.urediZaposlenik).subscribe((result :any)=>{
       this.httpKlijent.post(MyConfig.adresaServera + "/Zaposlenik/AddSlika/" + result, data).subscribe((result: any)=>{
-        this.obavjestenje = true;
-        this.closeModal = false;
-        this.obavjestenjeNaslov = "Ažuriranje podataka uspješno";
-        this.obavjestenjeSadrzaj = "Uspješno ste uredili podatke o dostavljaču "+ this.urediZaposlenik.ime+" "+this.urediZaposlenik.prezime;
+        this.prikaziObavjestenje("Ažuriranje podataka uspješno","Uspješno ste uredili podatke o zaposleniku " + this.urediZaposlenik.ime + " " + this.urediZaposlenik.prezime);
       });
-    });
+    });}
+    else{
+      this.prikaziObavjestenje("Neadekvatno ispunjena forma za promjenu ličnih podataka","Molimo ispunite sva obavezna polja, pa pokušajte ponovo.");
+    }
+  }
+  private prikaziObavjestenje(naslov : string, sadrzaj : string) {
+    this.obavjestenje = true;
+    this.closeModal = false;
+    this.obavjestenjeNaslov = naslov;
+    this.obavjestenjeSadrzaj = sadrzaj;
+  }
+  validirajFormu() : boolean{
+    // @ts-ignore
+    //var slika = document.getElementById("fajl-input").files[0];
+    return this.urediZaposlenik.korisnickoIme != null && this.urediZaposlenik.korisnickoIme?.length > 0
+      && this.urediZaposlenik.lozinka != null && this.urediZaposlenik.lozinka?.length > 0
+      && this.urediZaposlenik.ime != null && this.urediZaposlenik.ime?.length > 0
+      && this.urediZaposlenik.prezime != null && this.urediZaposlenik.prezime?.length > 0
+      && this.urediZaposlenik.email != null && this.urediZaposlenik.email?.length > 0
+      && this.urediZaposlenik.slika != null
+
+  }
+
+  provjeriPolje(polje: any) {
+    if (polje.invalid && (polje.dirty || polje.touched)){
+      if (polje.errors?.['required']){
+        return 'Niste popunili ovo polje!';
+      }
+      else {
+        return '';
+      }
+    }
+    return 'Obavezno polje za unos';
+  }
+  prikaziSakrij() {
+    this.fieldText = !this.fieldText;
   }
   animirajObavjestenje() {
     return this.closeModal == true? 'animate__animated animate__bounceOut' : 'animate__animated animate__bounceIn';
--- eRestoranAngular/src/app/zaposlenik/novi-zaposlenik/novi-zaposlenik.component.ts
+++ eRestoranAngular/src/app/zaposlenik/novi-zaposlenik/novi-zaposlenik.component.ts
@@ -16,6 +16,8 @@ export class NoviZaposlenikComponent implements OnInit {
   closeModal : boolean = false;
   obavjestenjeNaslov : string = "";
   obavjestenjeSadrzaj : string = "";
+
+  fieldText:boolean;//sakrivanje sifre
   constructor(private httpKlijent : HttpClient) { }
 
   ngOnInit(): void {
@@ -34,7 +36,7 @@ export class NoviZaposlenikComponent implements OnInit {
     }
   }
   posaljiPodatke() {
-    this.validirajFormu();
+    if(this.validirajFormu()){
     // @ts-ignore
     var file = document.getElementById("fajl-input").files[0];
     var data = new FormData();
@@ -42,15 +44,47 @@ export class NoviZaposlenikComponent implements OnInit {
     data.append("slikaZaposlenika", file);
     this.httpKlijent.post(MyConfig.adresaServera + "/Zaposlenik/Add", this.noviZaposlenik).subscribe((result : any)=>{
       this.httpKlijent.post(MyConfig.adresaServera + "/Zaposlenik/AddSlika/" + result, data).subscribe((result:any)=>{
-        this.obavjestenje = true;
-        this.closeModal = false;
-        this.obavjestenjeNaslov = "Ažuriranje podataka uspješno";
-        this.obavjestenjeSadrzaj = "Uspješno ste uredili podatke o zaposleniku "+ this.noviZaposlenik.ime+" "+this.noviZaposlenik.prezime;
+        this.prikaziObavjestenje("Dodavanje zaposlenika uspješno","Uspješno ste dodali podatke o zaposleniku " + this.noviZaposlenik.ime + " " + this.noviZaposlenik.prezime);
         this.ocistiFormu();
       });
-    });
+    });}
+    else{
+      this.prikaziObavjestenje("Neadekvatno ispunjena forma za promjenu ličnih podataka","Molimo ispunite sva obavezna polja, pa pokušajte ponovo.");
+    }
   }
 
+  private prikaziObavjestenje(naslov : string, sadrzaj : string) {
+    this.obavjestenje = true;
+    this.closeModal = false;
+    this.obavjestenjeNaslov = naslov;
+    this.obavjestenjeSadrzaj = sadrzaj;
+  }
+  validirajFormu() : boolean{
+    // @ts-ignore
+    var slika = document.getElementById("fajl-input").files[0];
+    return this.noviZaposlenik.username != null && this.noviZaposlenik.username?.length > 0
+      && this.noviZaposlenik.password != null && this.noviZaposlenik.password?.length > 0
+      && this.noviZaposlenik.ime != null && this.noviZaposlenik.ime?.length > 0
+      && this.noviZaposlenik.prezime != null && this.noviZaposlenik.prezime?.length > 0
+      && this.noviZaposlenik.email != null && this.noviZaposlenik.email?.length > 0
+      && slika!=null
+
+  }
+
+  provjeriPolje(polje: any) {
+    if (polje.invalid && (polje.dirty || polje.touched)){
+      if (polje.errors?.['required']){
+        return 'Niste popunili ovo polje!';
+      }
+      else {
+        return '';
+      }
+    }
+    return 'Obavezno polje za unos';
+  }
+  prikaziSakrij() {
+    this.fieldText = !this.fieldText;
+  }
 
   ocistiFormu(){
     this.noviZaposlenik.ime = null;
@@ -62,9 +96,7 @@ export class NoviZaposlenikComponent implements OnInit {
     document.getElementById("preview-slika").setAttribute("src","");
   }
 
-  private validirajFormu() {
 
-  }
   animirajObavjestenje() {
     return this.closeModal == true? 'animate__animated animate__bounceOut' : 'animate__animated animate__bounceIn';
   }
